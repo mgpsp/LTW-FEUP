d4d509c1ffb72fbdd52abe6e737c2d79d0ccdc46
Miguel Pereira <miguelgpspereira@gmail.com>
2015-10-30 17:18:21 +0000

    added javascript

---
+++ classes/javascript/form/script.js
@@ -0,0 +1,13 @@
+var products = ['ABCD', 'DEFG'];
+
+function loadDocument() {
+	window.alert("Hello World!");
+	var form = document.getElementById("products");
+	console.log(form.innerHTML);
+}
+
+function firstTagChild(elm) {
+	
+}
+
+window.addEventListener("load", loadDocument, false);
